3ea1ac2c10ae335c9da1542e1d7b37a8b4da54b3
archebus <joshua.devon@gmail.com>
2024-09-16 16:37:36 +1000

    Added a term not found flag to Task2

--- task2.js
+++ task2.js
@@ -10,14 +10,20 @@ fs.readFile(file, 'utf-8', (err, data) => {
 
     let lines = data.split('\n');
     let lineCount = 1;
+    let found = false;
 
     lines.forEach(line => {
         let startIndex = line.indexOf(term);
         while (startIndex !== -1) {
             console.log(`'${term}' found ${lineCount}:${startIndex + 1}`);
+            found = true;
             startIndex = line.indexOf(term, startIndex + term.length);
         }
         
         lineCount++;
     });
+
+    if (!found) {
+        console.log(`'${term}' not found in file.`)
+    }
 })
